96d54789862114b2cdb87ae9397e14f97ffcf939
renato-prof-tech <renatomo@professor.educacao.sp.gov.br>
2024-08-03 18:12:29 -0300

    Adicionando arquivos: html, css e js

---
+++ sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+    createCanvas(400, 400);
+     background(220);
+  }
+  
+  function draw() {
+    stroke("blue");
+    fill("red");
+    //console.log(mouseIsPressed);
+    if(mouseIsPressed){
+        rect(mouseX, mouseY,20,35);
+    }
+  }
